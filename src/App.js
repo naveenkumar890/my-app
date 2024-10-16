49348f01e38b15773119f09ba489373d085a1805
nagithub1 <chilakapatinagaraju2@gmail.com>
2024-10-16 18:23:54 +0530

    finalcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Formtext from './components/Formtext';
-import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
     <div className="container">
     <Formtext heading='React-friendly JSX code effortlessly'/>
     </div>
